d38fbd71dac9d1c3880b94cb43841493ed154727
jaworskig00 <2000grzegorz@gmail.com>
2023-02-07 21:25:49 +0100

    feat: add login/register components

---
+++ client/src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from "./Common/Button"
+export { default as Header } from "./Common/Header"
+export { default as Text } from "./Common/Text"
+
+export { default as LoginForm } from "./LoginForm/LoginForm"
