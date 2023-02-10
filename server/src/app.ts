f85ac570d0dada6a6c988bc0523007632726e91c
bunnv2 <bylic22@gmail.com>
2023-02-10 16:29:05 +0100

    Cors origin added

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,7 +18,12 @@ const pass = process.env.MONGO_PASSWORD
 const app = express()
 const port = 8080
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
